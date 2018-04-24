98e950e86ab42da85f2475949650cfbbeafb0a06
Cho KyongHo <pullip.cho@samsung.com>
2018-04-24 11:52:41 +0900

    moved

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ console.log("Kubia server starting...");
 var handler = function(request, response) {
     console.log("Received request from " + request.connection.remoteAddress);
     response.writeHead(200);
-    response.end("Your OS is " + os.platform() + "\n");
-    response.end("Your CPU is " + os.cpus()[0].model + "\n");
     response.end("You've hit " + os.hostname() + "\n");
+    response.end("Your CPU is " + os.cpus()[0].model + "\n");
+    response.end("Your OS is " + os.platform() + "\n");
 };
 
 var www = http.createServer(handler);
